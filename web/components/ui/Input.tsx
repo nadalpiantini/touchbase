6af2003b83e3ae1e7e2d3c2519dc99d655a8feb5
nadalpiantini <nadalpiantini@gmail.com>
2025-12-03 18:32:52 -0400

    perf: optimize Input component with React.memo

--- web/components/ui/Input.tsx
+++ web/components/ui/Input.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, forwardRef, useId } from 'react';
+import { InputHTMLAttributes, forwardRef, useId, memo } from 'react';
 import { cn } from '@/lib/utils';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -7,7 +7,7 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   helperText?: string;
 }
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
+const Input = memo(forwardRef<HTMLInputElement, InputProps>(
   ({ className, label, error, helperText, id, ...props }, ref) => {
     const generatedId = useId();
     const inputId = id || generatedId;
@@ -52,7 +52,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
       </div>
     );
   }
-);
+));
 
 Input.displayName = 'Input';
 
